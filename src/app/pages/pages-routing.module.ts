0ea810c160508ba2b78b221cb8bc59782db64209
xavier <xavier.cobos.29@hotmail.com>
2023-12-28 18:10:35 -0500

    Navegacion funcional

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumenComponent } from './resumen/resumen.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { HistoricoComponent } from './historico/historico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'resumen' },
+  { path: 'resumen', component: ResumenComponent },
+  { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+  { path: 'historico', component: HistoricoComponent },
+  { path: '**', redirectTo: 'resumen' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
